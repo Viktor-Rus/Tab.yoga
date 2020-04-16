ebb83e33158bc44386887cc1aa30f694ff3a2f8b
Viktor Urusov <v.urusov@intec-balance.ru>
2020-04-16 22:32:40 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -33,4 +33,57 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+
+    let deadline = '2020-04-17';
+
+    function getTimeReaming(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+
+    }
+
+    function setClock(id, endtime) {
+
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            function updateClock() {
+                let t = getTimeReaming(endtime);
+
+                function addZero(num) {
+                    if (num<=9) {
+                        return '0' + num;
+                    } else return num;
+                }
+
+                hours.textContent = addZero(t.hours);
+                minutes.textContent = addZero(t.minutes);
+                seconds.textContent = addZero(t.seconds);
+
+                if (t.total<=0){
+                    hours.textContent = '00';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
+                    clearInterval(timeInterval);
+                }
+            }
+    }
+
+    setClock('timer', deadline);
+
+
+
 });
